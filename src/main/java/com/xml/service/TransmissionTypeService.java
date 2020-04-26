0921c4a86f053a4b0b433b2a3f66f6d44e6df5df
igorMales98 <igor_malesevic@yahoo.com>
2020-04-26 03:21:03 +0200

    Get all fuel and transmission types.

---
+++ src/main/java/com/xml/service/TransmissionTypeService.java
@@ -0,0 +1,10 @@
+package com.xml.service;
+
+import com.xml.model.TransmissionType;
+
+import java.util.List;
+
+public interface TransmissionTypeService {
+
+    List<TransmissionType> getAll();
+}
