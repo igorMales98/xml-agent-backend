d95e9b3bef4382b78f69f13c39eb61aa35123443
igorMales98 <igor_malesevic@yahoo.com>
2020-05-11 14:07:22 +0200

    If customer went over allowed distance, make additional bill for him to pay.

--- src/main/java/com/xml/model/Customer.java
+++ src/main/java/com/xml/model/Customer.java
@@ -5,6 +5,8 @@ import org.hibernate.validator.constraints.Range;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.Set;
 
 @Entity
 @DiscriminatorValue("CUSTOMER")
@@ -13,4 +15,8 @@ public class Customer extends User {
     @Column
     @Range(min = 0, max = 3)
     private short advertisementsPosted;
+
+    @OneToMany(mappedBy = "customer")
+    private Set<AdditionalBill> additionalBills;
+
 }
